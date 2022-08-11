9a95b4765fe1880ec4b89c4dd45dd8e698b0f5bd
Brayam1993 <argofrogg@gmail.com>
2022-08-11 16:33:35 -0500

    test connection to postgres

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+
 import app from './app.js';
+import { sequelize } from "./database/database.js";
 
-app.listen(4000);
+async function main(){
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully");
+        app.listen(4000);
+        console.log('Server is listening on port', 4000);
+    }catch(e){
+        console.log("Unable to connect to the database:", e);
+    }
 
-console.log('Server is listening on port', 4000);
+};
 
+main();
