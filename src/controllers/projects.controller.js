d9ccc782a536344470416bf5c47c7b6a1ef6fa30
Brayam1993 <argofrogg@gmail.com>
2022-08-12 09:39:19 -0500

    added end point update project

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -13,7 +13,7 @@ export const getProjects = async (req, res) => {
 
 export const createProjects = async (req, res) => {
     const {name, priority, description} = req.body;
-    
+
     try{
         const newProject = await Project.create({
             name,
@@ -24,5 +24,36 @@ export const createProjects = async (req, res) => {
         res.json(newProject)
     }catch(e){
         return res.status(500).json({message: error.message});
+    }
+}
+
+export const updateProject = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const { name, priority, description } = req.body;
+
+        const project = await Project.findByPk(id);
+        project.name = name;
+        project.priority = priority;
+        project.description = description;
+        await project.save();
+
+        res.json(project);
+    }catch(e){
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+export const deleteProject = async (req, res) => {
+    try{
+        const { id } = req.params;
+        await Project.destroy({
+            where: {
+                id,
+            },
+        });
+        res.sendStatus(204);
+    }catch(e){
+        return res.status(500).json({ message: error.message });
     }
 }
--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -2,14 +2,16 @@ import { Router } from "express";
 import {
     createProjects,
     getProjects,
+    updateProject,
+    deleteProject
 } from "../controllers/projects.controller.js"
 
 const router = Router();
 
 router.get("/projects", getProjects);
 router.post("/projects", createProjects);
-router.put("/projects/:id");
-router.delete("/projects/:id");
+router.put("/projects/:id", updateProject);
+router.delete("/projects/:id", deleteProject);
 router.get("/projects/:id");
 
 export default router;
